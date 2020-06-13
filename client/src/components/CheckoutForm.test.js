750dedff18c8bf18980f3dcaa6aaa956c76052d1
venegasgabby76 <gabbyvenegas76@yahoo.com>
2020-06-12 19:03:46 -0700

    finially got my testing to work

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -5,51 +5,50 @@ import CheckoutForm from "./CheckoutForm";
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
-    const { getByText} = render(<CheckoutForm />);
+    const { getByText } = render(<CheckoutForm />)
 
     const header = getByText(/checkout form/i)
 
-    expect(header).toBeInDocument()
+    expect(header).toBeInTheDocument()
+
 });
 
 test("form shows success message on submit with form details", () => {
-    
+   
+    render(<CheckoutForm />)
 
-    const firstNameInput = screen.getByLabelText(/firstName/i);
-    const lastNameInput = screen.getByLabelText(/lastName/i);
-    const addressInput = screen.getByLabelText(/address/i);
-    const cityInput = screen.getByLabelText(/city/i);
-    const stateInput = screen.getByLabelText(/state/i);
-    const zipInput = screen.getByLabelText(/zip/i);
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    const lastNameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
 
-    
-    fireEvent.change(firstNameInput, { target: { value: 'Gabby' } });
-    fireEvent.change(lastNameInput, { target: { value: 'Venegas' } });
-    fireEvent.change(addressInput, { target: { value: '18642 Nordhoff St' } });
-    fireEvent.change(cityInput, { target: { value: 'Northridge' } });
-    fireEvent.change(stateInput, { target: { value: 'California' } });
-    fireEvent.change(zipInput, { target: { value: '91324' } });
-
-    expect(firstNameInput.value).toBe('Gabby');
-    expect(lastNameInput.value).toBe('Venegas');
-    expect(addressInput.value).toBe('18642 Nordhoff St');
-    expect(cityInput.value).toBe('Northridge');
-    expect(stateInput.value).toBe('California');
-    expect(zipInput.value).toBe('91324');
+    fireEvent.change(firstNameInput, { target: { value: 'Gabby' } })
+    fireEvent.change(lastNameInput, { target: { value: 'Venegas' } })
+    fireEvent.change(addressInput, { target: { value: '2727 St' } })
+    fireEvent.change(cityInput, { target: { value: 'Shibuya' } })
+    fireEvent.change(stateInput, { target: { value: 'CA' } })
+    fireEvent.change(zipInput, { target: { value: '91324' } })
 
 
-    //for the button
-    const clickButton = screen.getByText('Checkout')
-    fireEvent.click(clickButton)
+    expect(firstNameInput.value).toBe('Gabby')
+    expect(lastNameInput.value).toBe('Venegas')
+    expect(addressInput.value).toBe('2727 St')
+    expect(cityInput.value).toBe('Shibuya')
+    expect(stateInput.value).toBe('CA')
+    expect(zipInput.value).toBe('91324')
 
-    const yourName = screen.getByText(/gabby/i)
-    expect(yourName).toBeInDocument
 
-    //plant submittion
-    const successPlants = screen.getByTestId(/successMessgae/i)
-    expect(successPlants).toBeInDocument
+    const checkoutButton = screen.getByText('Checkout')
+    fireEvent.click(checkoutButton)
 
-    
+
+    const yourName= screen.getByText(/gabby/i)
+    expect(yourName).toBeInTheDocument()
+
+
+    const successPlants = screen.getByTestId(/successMessage/i)
+    expect(successPlants).toBeInTheDocument()
 
 });
-
